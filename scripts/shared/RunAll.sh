c212f9d742cfb9808b7e1e1e16a85ee9b0b38e4f
mjo22 <mjobrien22@gmail.com>
2021-01-28 16:26:44 -0500

    Add new RunAll.sh script

---
+++ scripts/shared/RunAll.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Convenience script to run all files in a certain class
+
+sourcedir=$(dirname -- $BASH_SOURCE)
+label=$1
+
+temp=($@)
+args="${temp[@]:1}"
+
+if [[ $label == "" ]]; then
+    echo "Please enter a label for the file class"
+    exit
+fi
+
+find -L $sourcedir -maxdepth 1 -mindepth 1 -name "${label}*.toml" -xtype l -exec bash $sourcedir/Run.sh $sourcedir/${label}Data.py {} $args \;
+
+find $sourcedir -maxdepth 1 -mindepth 1 -name "${label}*.toml" -type f -exec bash $sourcedir/Run.sh $sourcedir/${label}Data.py {} $args \;
