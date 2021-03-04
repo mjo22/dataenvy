be948df5935b0648cd5fc5734ddacea2835d8746
mjo22 <mjobrien22@gmail.com>
2021-03-03 22:28:44 -0500

    Cleaning output scripts should be a script not command

--- Permission.sh
+++ Permission.sh
@@ -3,7 +3,7 @@
 # Make scripts into executables
 
 
-commands=("setup-global" "setup-module" "setup-dataset" "sync-dataset" "write-dataset" "delete-dataset" "clean-out" "submit-script" "submit-modules" "submit-base")
+commands=("setup-global" "setup-module" "setup-dataset" "sync-dataset" "write-dataset" "delete-dataset" "submit-script" "submit-modules" "submit-base")
 
 for cmd in "${commands[@]}"; do
     chmod +x $cmd
